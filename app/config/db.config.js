f44894f40a80d1e113312f9ab5024af5ef43937f
Luky Mulana <49505534+lukymulana@users.noreply.github.com>
2023-01-18 14:43:25 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "10.19.16.21",
+  HOST: "10.19.23.18",
   PORT: "1433",
   USER: "sa",
-  PASSWORD: "User@new1",
-  DB: "prod_control",
+  PASSWORD: "admin",
+  DB: "control",
   dialect: "mssql",
   pool: {
     max: 5,
